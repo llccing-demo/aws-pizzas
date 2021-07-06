72d67f6b01d84239a088b280a28c77588e710001
小新 <lcf33123@gmail.com>
2021-07-06 22:01:29 +0800

    feat: save data in dynamodb

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,8 +1,30 @@
-function createOrder(order) {
-  if (!order || !order.pizzaId || !order.address) {
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+function createOrder(request) {
+  if (!request || !request.pizzaId || !request.address) {
     throw new Error('订购 pizza 需要提供地址和口味，这样你才能收到 pizza 啊')
-    return {}
   }
+
+  return docClient
+    .put({
+      TableName: 'pizza-orders',
+      Item: {
+        orderId: 'some-id',
+        pizza: request.pizza,
+        address: request.address,
+        orderStatus: 'pending',
+      },
+    })
+    .promise()
+    .then((res) => {
+      console.log('订单已保存', res)
+      return res
+    })
+    .catch((saveError) => {
+      console.log('发生错误了', saveError)
+      throw saveError
+    })
 }
 
 module.exports = createOrder
